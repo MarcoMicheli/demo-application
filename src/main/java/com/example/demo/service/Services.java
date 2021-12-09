ff9336391e5eb7ea43df72adb170f052b4e677d6
DIR\alfonso.buononato <alfonsobuononato@gmail.com>
2021-12-09 12:42:42 +0100

    new refactoring create class service

---
+++ src/main/java/com/example/demo/service/Services.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import javax.ws.rs.QueryParam;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+@Service
+public class Services {
+	
+	public String test(String string, String control) {
+		if(control.toLowerCase().equals("upper-case")) {
+			string = string.toUpperCase();
+			return string;
+		}else if(control.toLowerCase().equals("lower-case")) {
+			string = string.toLowerCase();
+			return string;
+		}
+		return string;
+	}
+}
