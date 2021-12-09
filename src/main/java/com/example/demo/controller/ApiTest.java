9b806b02b5fe3d5e6167b3e706eba0c5ac00b745
DIR\alfonso.buononato <alfonsobuononato@gmail.com>
2021-12-09 12:42:42 +0100

    new refactoring create class service
    (cherry picked from commit ff9336391e5eb7ea43df72adb170f052b4e677d6)

--- src/main/java/com/example/demo/controller/ApiTest.java
+++ src/main/java/com/example/demo/controller/ApiTest.java
@@ -5,24 +5,22 @@ package com.example.demo.controller;
 import javax.websocket.server.PathParam;
 import javax.ws.rs.QueryParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.service.Services;
+
 
 @RestController
 public class ApiTest {
-	
+	@Autowired
+	private Services service;
 	@GetMapping("/firstApi/{string}")
-	public String test(@PathVariable String string, @QueryParam("control") String control) {
-		if(control.toLowerCase().equals("upper-case")) {
-			string = string.toUpperCase();
-			return string;
-		}else if(control.toLowerCase().equals("lower-case")) {
-			string = string.toLowerCase();
-			return string;
-		}
-		return string;
+	public String newTest(@PathVariable String string, @QueryParam("control") String control) {
+		return service.test(string, control);
 	}
+	
 }
