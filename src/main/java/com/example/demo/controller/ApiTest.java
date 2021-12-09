b984c0b4a72baa1737d05ab72410913b0862c8ed
DIR\alfonso.buononato <alfonsobuononato@gmail.com>
2021-12-09 11:06:47 +0100

    test creation on get request

--- src/main/java/com/example/demo/controller/ApiTest.java
+++ src/main/java/com/example/demo/controller/ApiTest.java
@@ -1,13 +1,28 @@
 package com.example.demo.controller;
 
+
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.QueryParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class ApiTest {
-	@GetMapping("/firstApi")
-	public String test() {
-		return "Hello from first api.";
-	}
 	
+	@GetMapping("/firstApi/{string}")
+	public String test(@PathVariable String string, @QueryParam("control") String control) {
+		if(control.equals("UPPER-CASE")) {
+			string = string.toUpperCase();
+			return string;
+		}else if(control.equals("LOWER-CASE")) {
+			string = string.toLowerCase();
+			return string;
+		}
+		return string;
+	}
 }
